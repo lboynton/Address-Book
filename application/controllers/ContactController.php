ad7351c721d05ab03619c7baa5d6b96c6585b1e1
lboynton <notlee@gmail.com>
2009-09-30 17:05:31 +0100

    Added add action and view for contacts

--- application/controllers/ContactController.php
+++ application/controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 class ContactController extends Zend_Controller_Action
 {
+
     public function init()
     {
         /* Initialize action controller here */
@@ -11,5 +12,10 @@ class ContactController extends Zend_Controller_Action
     {
         $contact = new Default_Model_Contact();
         $this->view->entries = $contact->fetchAll();
+    }
+
+    public function addAction()
+    {
+        // action body
     }
 }
