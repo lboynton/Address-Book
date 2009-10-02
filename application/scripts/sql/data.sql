d982f222e0e8942e91e35f8af85f5a06c50fa4ff
lboynton <notlee@gmail.com>
2009-10-02 11:37:54 +0100

    Updated data SQL script to take into account new field: town

--- application/scripts/sql/data.sql
+++ application/scripts/sql/data.sql
@@ -4,4 +4,4 @@ truncate contacts;
 
 -- insert some test data
 INSERT INTO address_books (name) VALUES ('Home'),('Work');
-INSERT INTO contacts VALUES (1,1,'Bob','Bobbington','1 Bob Road','','Bobtown','Bobland','PO12UP','01234 567890','09876 543210',NULL,NULL,'bob@bobmail.com');
+INSERT INTO contacts VALUES (1,1,'Bob','Bobbington','1 Bob Road','','Bobtown','Bobcounty','Bobland','PO12UP','01234 567890','09876 543210',NULL,NULL,'bob@bobmail.com');
